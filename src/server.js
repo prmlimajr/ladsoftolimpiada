4fb2837f8e3d2e11ef271f272298b63f64169bc0
Paulo Lima <prmlimajr@hotmail.com>
2020-10-06 22:11:22 -0300

    teste cors

--- src/server.js
+++ src/server.js
@@ -8,6 +8,17 @@ const PORT = process.env.PORT || 3333;
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://ladsoft-olimpiada.herokuapp.com/'
+  );
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.listen(PORT, () => {
   Logger.success(`Conectado na porta ${PORT}`);
 });
