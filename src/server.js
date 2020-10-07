f99b285feff20ef758f907e37ba8881dfe0eb05f
Paulo Lima <prmlimajr@hotmail.com>
2020-10-06 21:08:58 -0300

    port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import Logger from './lib/logger';
 
 import app from './app';
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
   Logger.success(`Conectado na porta ${PORT}`);
